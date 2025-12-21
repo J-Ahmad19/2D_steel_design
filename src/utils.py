c3db2a6ff4511d75394cbf5195ee93cb87e95d85
J-Ahmad19 <jawwadahmad568@gmail.com>
2025-12-21 14:14:10 +0530

    utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
-# src/utils.py
 import re
 
 def sanitize_filename(name: str) -> str:
-    """Sanitizes a string for use as a filename."""
-    safe = re.sub(r"[^\w\-.]", "_", str(name))
-    return safe[:200]
+    """Removes illegal characters from filenames."""
+    # Replace slashes and other bad chars with underscores
+    s = str(name).strip().replace('/', '-').replace('\\', '-')
+    return re.sub(r'[^\w\-.]', '_', s)
